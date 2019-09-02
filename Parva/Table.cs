2b9b6b9bdb34c2d3a88b249a7f27313956a1597a
Matthew Lewis <G17L3581@ict.ru.ac.za>
2019-09-02 18:47:40 +0200

    Completed the Table.cs file, shot for the help Makkie boy (hope holidays is good <3)

--- Parva/Table.cs
+++ Parva/Table.cs
@@ -16,22 +16,41 @@ namespace Parva {
   } // Entry
 
   class Table {
-    static List<Entry> list = new List<Entry>();
+    static List<Entry> theList = new List<Entry>();
 
     public static void ClearTable() {
-    // Clears cross-reference table
-    // ...
+            // Clears cross-reference table
+            theList.Clear();
     } // Table.ClearTable
 
     public static void AddRef(string name, bool declared, int lineRef) {
-    // Enters name if not already there, and then adds another line reference (negative
-    // if at a declaration point in the original source program)
-    // ...
+            // Enters name if not already there, and then adds another line reference (negative
+            // if at a declaration point in the original source program)
+            if (!declared) 
+            { //Not declared (YET)
+                //Adds name to the theList  and then adds the lineRef to the refs list.
+                theList.Add(new Entry(name));
+                theList[theList.Count() - 1].refs.Add(lineRef);
+            } else
+            {  //Already declared
+                for (int i = 0; i < theList.Count(); i++)
+                    if(theList[i].name == name)
+                        theList[i].refs.Add(lineRef);
+            }
+                
     } // Table.AddRef
 
     public static void PrintTable() {
-    // Prints out all references in the table (eliminate duplicates line numbers)
-    //...
+    // Prints out all references in the table (eliminate duplicate line numbers)
+            string display = "";
+            foreach (Entry x in theList)
+            {
+                display += x.name + "\t\t\t";
+                display += "-" + x.refs[0] + "\t";
+                for (int i = 1; i < x.refs.Count(); i++)
+                    display += x.refs[i] + "\t";
+                display += "\n";
+            }
     } // Table.PrintTable
 
   } // Table
