930cd9841fdd2c2908e2ca7cc26cce95c0d7c803
Matthew Lewis <G17L3581@ict.ru.ac.za>
2019-09-04 14:11:04 -1000

    Mak.

---
+++ Parser.cs
@@ -0,0 +1,603 @@
+using Library;  //a = 5 + 5
+
+
+
+using System;
+using System.IO;
+using System.Text;
+
+namespace Calc {
+
+public class Parser {
+	public const int _EOF = 0;
+	public const int _number = 1;
+	public const int _identifier = 2;
+	// terminals
+	public const int EOF_SYM = 0;
+	public const int number_Sym = 1;
+	public const int identifier_Sym = 2;
+	public const int quit_Sym = 3;
+	public const int equal_Sym = 4;
+	public const int semicolon_Sym = 5;
+	public const int print_Sym = 6;
+	public const int comma_Sym = 7;
+	public const int barbar_Sym = 8;
+	public const int andand_Sym = 9;
+	public const int plus_Sym = 10;
+	public const int minus_Sym = 11;
+	public const int bang_Sym = 12;
+	public const int true_Sym = 13;
+	public const int false_Sym = 14;
+	public const int lparen_Sym = 15;
+	public const int rparen_Sym = 16;
+	public const int star_Sym = 17;
+	public const int slash_Sym = 18;
+	public const int percent_Sym = 19;
+	public const int less_Sym = 20;
+	public const int lessequal_Sym = 21;
+	public const int greater_Sym = 22;
+	public const int greaterequal_Sym = 23;
+	public const int equalequal_Sym = 24;
+	public const int bangequal_Sym = 25;
+	public const int NOT_SYM = 26;
+	// pragmas
+
+	public const int maxT = 26;
+
+	const bool T = true;
+	const bool x = false;
+	const int minErrDist = 2;
+
+	public static Token token;    // last recognized token   /* pdt */
+	public static Token la;       // lookahead token
+	static int errDist = minErrDist;
+
+	static string display = "";
+
+static int ToInt(bool b) {
+// return 0 or 1 according as b is false or true
+  return b ? 1 : 0;
+} // ToInt
+
+static bool ToBool(int i) {
+// return false or true according as i is 0 or 1
+  return i == 0 ? false : true;
+} // ToBool
+
+static int Add(int a, int b, string op) {
+	if(op == "+")
+		return a+b;
+	else if (op == "-")
+		return a-b;
+	else
+		return -1;//throw error (not an ideal solution, WILL fix)
+}
+
+static int Mult(int a, int b, string op) {
+	if(op == "%")
+		return a%b;
+	else if (op == "*")
+		return a*b;
+	else if (op == "/")
+		return a/b;
+	else
+		return -1;//throw error (not an ideal solution, WILL fix)
+}
+
+static int Or(int a, int b) {
+	 bool c = ToBool(a);
+	 bool d = ToBool(b);
+	 bool e = c || d;
+	 return ToInt(e);
+}
+
+static int And(int a, int b) {
+	 bool c = ToBool(a);
+	 bool d = ToBool(b);
+	 bool e = c && d;
+	 return ToInt(e);
+}
+
+static int Eql(int a, int b, string op){
+	 if ( op == "==" ) //oof, inception vibes.
+		return ToInt((a == b));
+	 else if (op == "!=")
+		return ToInt((a != b));
+	 else
+		return -1;//throw error (not an ideal solution, WILL fix)
+		
+}
+
+static int Rel(int a, int b, string op){
+	if ( op == "<")
+		return ToInt(a < b);
+	else if (op == "<=")
+		return ToInt(a <= b);
+	else if (op == ">")
+		return ToInt(a > b);
+	else if (op == ">=")
+		return ToInt(a >= b);
+	else
+		return -1;//throw error (not an ideal solution, WILL fix)
+}
+
+static int Not(int a){
+	bool b = ToBool(a);
+	bool c = !b;
+	return ToInt(c);
+}
+
+
+
+	static void SynErr (int n) {
+		if (errDist >= minErrDist) Errors.SynErr(la.line, la.col, n);
+		errDist = 0;
+	}
+
+	public static void SemErr (string msg) {
+		if (errDist >= minErrDist) Errors.Error(token.line, token.col, msg); /* pdt */
+		errDist = 0;
+	}
+
+	public static void SemError (string msg) {
+		if (errDist >= minErrDist) Errors.Error(token.line, token.col, msg); /* pdt */
+		errDist = 0;
+	}
+
+	public static void Warning (string msg) { /* pdt */
+		if (errDist > minErrDist) Errors.Warn(token.line, token.col, msg);
+		errDist = 2; //++ 2009/11/04
+	}
+
+	public static bool Successful() { /* pdt */
+		return Errors.count == 0;
+	}
+
+	public static string LexString() { /* pdt */
+		return token.val;
+	}
+
+	public static string LookAheadString() { /* pdt */
+		return la.val;
+	}
+
+	static void Get () {
+		for (;;) {
+			token = la; /* pdt */
+			la = Scanner.Scan();
+			if (la.kind <= maxT) { ++errDist; break; }
+
+			la = token; /* pdt */
+		}
+	}
+
+	static void Expect (int n) {
+		if (la.kind==n) Get(); else { SynErr(n); }
+	}
+
+	static bool StartOf (int s) {
+		return set[s, la.kind];
+	}
+
+	static void ExpectWeak (int n, int follow) {
+		if (la.kind == n) Get();
+		else {
+			SynErr(n);
+			while (!StartOf(follow)) Get();
+		}
+	}
+
+	static bool WeakSeparator (int n, int syFol, int repFol) {
+		bool[] s = new bool[maxT+1];
+		if (la.kind == n) { Get(); return true; }
+		else if (StartOf(repFol)) return false;
+		else {
+			for (int i=0; i <= maxT; i++) {
+				s[i] = set[syFol, i] || set[repFol, i] || set[0, i];
+			}
+			SynErr(n);
+			while (!s[la.kind]) Get();
+			return StartOf(syFol);
+		}
+	}
+
+	static void Calc() {
+		int Value = 0;
+		while (la.kind == identifier_Sym || la.kind == print_Sym) {
+			if (la.kind == print_Sym) {
+				Print(out Value);
+			} else {
+				Assignment(out Value);
+				Console.WriteLine(Value.ToString());
+			}
+		}
+		Expect(quit_Sym);
+	}
+
+	static void Print(out int Value) {
+		Expect(print_Sym);
+		Expression(out Value);
+		Console.WriteLine(Value.ToString());
+		while (WeakSeparator(comma_Sym, 1, 2)) {
+			Expression(out Value);
+			Console.WriteLine(Value.ToString());
+		}
+		while (!(la.kind == EOF_SYM || la.kind == semicolon_Sym)) {SynErr(27); Get();}
+		Expect(semicolon_Sym);
+	}
+
+	static void Assignment(out int Value) {
+		Variable();
+		Expect(equal_Sym);
+		Expression(out Value);
+		while (!(la.kind == EOF_SYM || la.kind == semicolon_Sym)) {SynErr(28); Get();}
+		Expect(semicolon_Sym);
+	}
+
+	static void Variable() {
+		Expect(identifier_Sym);
+	}
+
+	static void Expression(out int Value) {
+		AndExp(out Value);
+		while (la.kind == barbar_Sym) {
+			Get();
+			int otherValue = 0;
+			AndExp(out otherValue);
+			Value = Or(Value, otherValue);
+		}
+	}
+
+	static void AndExp(out int Value) {
+		EqlExp(out Value);
+		while (la.kind == andand_Sym) {
+			Get();
+			int otherValue = 0;
+			EqlExp(out otherValue);
+			Value = And(Value, otherValue);
+		}
+	}
+
+	static void EqlExp(out int Value) {
+		RelExp(out Value);
+		while (la.kind == equalequal_Sym || la.kind == bangequal_Sym) {
+			string op = "";
+			EqlOp(out op);
+			int otherValue = 0;
+			RelExp(out otherValue);
+			Value = Eql(Value,otherValue, op);
+		}
+	}
+
+	static void RelExp(out int Value) {
+		AddExp(out Value);
+		if (StartOf(3)) {
+			string op = "";
+			RelOp(out op);
+			int otherValue = 0;
+			AddExp(out otherValue);
+			Value = Rel(Value,otherValue, op);
+		}
+	}
+
+	static void EqlOp(out string op) {
+		op = "";
+		if (la.kind == equalequal_Sym) {
+			Get();
+			op = token.val;
+		} else if (la.kind == bangequal_Sym) {
+			Get();
+			op = token.val;
+		} else SynErr(29);
+	}
+
+	static void AddExp(out int Value) {
+		MultExp(out Value);
+		while (la.kind == plus_Sym || la.kind == minus_Sym) {
+			string op = "";
+			AddOp(out op);
+			int otherValue = 0;
+			MultExp(out otherValue);
+			Value =  Add(Value, otherValue,op);
+		}
+	}
+
+	static void RelOp(out string op) {
+		op = "";
+		if (la.kind == less_Sym) {
+			Get();
+			op = token.val;
+		} else if (la.kind == lessequal_Sym) {
+			Get();
+			op = token.val;
+		} else if (la.kind == greater_Sym) {
+			Get();
+			op = token.val;
+		} else if (la.kind == greaterequal_Sym) {
+			Get();
+			op = token.val;
+		} else SynErr(30);
+	}
+
+	static void MultExp(out int Value) {
+		UnaryExp(out Value);
+		while (la.kind == star_Sym || la.kind == slash_Sym || la.kind == percent_Sym) {
+			string op = "";
+			MulOp(out op);
+			int otherValue = 0;
+			UnaryExp(out otherValue);
+			Value = Mult(Value, otherValue, op);
+		}
+	}
+
+	static void AddOp(out string op) {
+		op = "";
+		if (la.kind == plus_Sym) {
+			Get();
+			op = token.val;
+		} else if (la.kind == minus_Sym) {
+			Get();
+			op = token.val;
+		} else SynErr(31);
+	}
+
+	static void UnaryExp(out int Value) {
+		Value = 0; int otherValue = 0;
+		if (StartOf(4)) {
+			Factor(out Value);
+		} else if (la.kind == plus_Sym) {
+			Get();
+			UnaryExp(out otherValue);
+			Value = Add(Value, otherValue, "+");
+		} else if (la.kind == minus_Sym) {
+			Get();
+			UnaryExp(out otherValue);
+			Value = Add(Value, otherValue, "-");
+		} else if (la.kind == bang_Sym) {
+			Get();
+			UnaryExp(out otherValue);
+			Value = Not(otherValue);
+		} else SynErr(32);
+	}
+
+	static void MulOp(out string op) {
+		op = "";
+		if (la.kind == star_Sym) {
+			Get();
+			op = token.val;
+		} else if (la.kind == slash_Sym) {
+			Get();
+			op = token.val;
+		} else if (la.kind == percent_Sym) {
+			Get();
+			op = token.val;
+		} else SynErr(33);
+	}
+
+	static void Factor(out int Value) {
+		Value = 0;
+		if (la.kind == identifier_Sym) {
+			Variable();
+		} else if (la.kind == number_Sym) {
+			Number(out Value);
+		} else if (la.kind == true_Sym) {
+			Get();
+			Value = 1;
+		} else if (la.kind == false_Sym) {
+			Get();
+			Value = 0;
+		} else if (la.kind == lparen_Sym) {
+			Get();
+			Expression(out Value);
+			Expect(rparen_Sym);
+		} else SynErr(34);
+	}
+
+	static void Number(out int Value) {
+		Expect(number_Sym);
+		Value =  Convert.ToInt32(token.val);
+	}
+
+
+
+	public static void Parse() {
+		la = new Token();
+		la.val = "";
+		Get();
+		Calc();
+		Expect(EOF_SYM);
+
+	}
+
+	static bool[,] set = {
+		{T,x,x,x, x,T,x,x, x,x,x,x, x,x,x,x, x,x,x,x, x,x,x,x, x,x,x,x},
+		{x,T,T,x, x,x,x,x, x,x,T,T, T,T,T,T, x,x,x,x, x,x,x,x, x,x,x,x},
+		{x,x,x,x, x,T,x,x, x,x,x,x, x,x,x,x, x,x,x,x, x,x,x,x, x,x,x,x},
+		{x,x,x,x, x,x,x,x, x,x,x,x, x,x,x,x, x,x,x,x, T,T,T,T, x,x,x,x},
+		{x,T,T,x, x,x,x,x, x,x,x,x, x,T,T,T, x,x,x,x, x,x,x,x, x,x,x,x}
+
+	};
+
+} // end Parser
+
+/* pdt - considerable extension from here on */
+
+public class ErrorRec {
+	public int line, col, num;
+	public string str;
+	public ErrorRec next;
+
+	public ErrorRec(int l, int c, string s) {
+		line = l; col = c; str = s; next = null;
+	}
+
+} // end ErrorRec
+
+public class Errors {
+
+	public static int count = 0;                                     // number of errors detected
+	public static int warns = 0;                                     // number of warnings detected
+	public static string errMsgFormat = "file {0} : ({1}, {2}) {3}"; // 0=file 1=line, 2=column, 3=text
+	static string fileName = "";
+	static string listName = "";
+	static bool mergeErrors = false;
+	static StreamWriter mergedList;
+
+	static ErrorRec first = null, last;
+	static bool eof = false;
+
+	static string GetLine() {
+		char ch, CR = '\r', LF = '\n';
+		int l = 0;
+		StringBuilder s = new StringBuilder();
+		ch = (char) Buffer.Read();
+		while (ch != Buffer.EOF && ch != CR && ch != LF) {
+			s.Append(ch); l++; ch = (char) Buffer.Read();
+		}
+		eof = (l == 0 && ch == Buffer.EOF);
+		if (ch == CR) {  // check for MS-DOS
+			ch = (char) Buffer.Read();
+			if (ch != LF && ch != Buffer.EOF) Buffer.Pos--;
+		}
+		return s.ToString();
+	}
+
+	static void Display (string s, ErrorRec e) {
+		mergedList.Write("**** ");
+		for (int c = 1; c < e.col; c++)
+			if (s[c-1] == '\t') mergedList.Write("\t"); else mergedList.Write(" ");
+		mergedList.WriteLine("^ " + e.str);
+	}
+
+	public static void Init (string fn, string dir, bool merge) {
+		fileName = fn;
+		listName = dir + "listing.txt";
+		mergeErrors = merge;
+		if (mergeErrors)
+			try {
+				mergedList = new StreamWriter(new FileStream(listName, FileMode.Create));
+			} catch (IOException) {
+				Errors.Exception("-- could not open " + listName);
+			}
+	}
+
+	public static void Summarize () {
+		if (mergeErrors) {
+			mergedList.WriteLine();
+			ErrorRec cur = first;
+			Buffer.Pos = 0;
+			int lnr = 1;
+			string s = GetLine();
+			while (!eof) {
+				mergedList.WriteLine("{0,4} {1}", lnr, s);
+				while (cur != null && cur.line == lnr) {
+					Display(s, cur); cur = cur.next;
+				}
+				lnr++; s = GetLine();
+			}
+			if (cur != null) {
+				mergedList.WriteLine("{0,4}", lnr);
+				while (cur != null) {
+					Display(s, cur); cur = cur.next;
+				}
+			}
+			mergedList.WriteLine();
+			mergedList.WriteLine(count + " errors detected");
+			if (warns > 0) mergedList.WriteLine(warns + " warnings detected");
+			mergedList.Close();
+		}
+		switch (count) {
+			case 0 : Console.WriteLine("Parsed correctly"); break;
+			case 1 : Console.WriteLine("1 error detected"); break;
+			default: Console.WriteLine(count + " errors detected"); break;
+		}
+		if (warns > 0) Console.WriteLine(warns + " warnings detected");
+		if ((count > 0 || warns > 0) && mergeErrors) Console.WriteLine("see " + listName);
+	}
+
+	public static void StoreError (int line, int col, string s) {
+		if (mergeErrors) {
+			ErrorRec latest = new ErrorRec(line, col, s);
+			if (first == null) first = latest; else last.next = latest;
+			last = latest;
+		} else Console.WriteLine(errMsgFormat, fileName, line, col, s);
+	}
+
+	public static void SynErr (int line, int col, int n) {
+		string s;
+		switch (n) {
+			case 0: s = "EOF expected"; break;
+			case 1: s = "number expected"; break;
+			case 2: s = "identifier expected"; break;
+			case 3: s = "\"quit\" expected"; break;
+			case 4: s = "\"=\" expected"; break;
+			case 5: s = "\";\" expected"; break;
+			case 6: s = "\"print\" expected"; break;
+			case 7: s = "\",\" expected"; break;
+			case 8: s = "\"||\" expected"; break;
+			case 9: s = "\"&&\" expected"; break;
+			case 10: s = "\"+\" expected"; break;
+			case 11: s = "\"-\" expected"; break;
+			case 12: s = "\"!\" expected"; break;
+			case 13: s = "\"true\" expected"; break;
+			case 14: s = "\"false\" expected"; break;
+			case 15: s = "\"(\" expected"; break;
+			case 16: s = "\")\" expected"; break;
+			case 17: s = "\"*\" expected"; break;
+			case 18: s = "\"/\" expected"; break;
+			case 19: s = "\"%\" expected"; break;
+			case 20: s = "\"<\" expected"; break;
+			case 21: s = "\"<=\" expected"; break;
+			case 22: s = "\">\" expected"; break;
+			case 23: s = "\">=\" expected"; break;
+			case 24: s = "\"==\" expected"; break;
+			case 25: s = "\"!=\" expected"; break;
+			case 26: s = "??? expected"; break;
+			case 27: s = "this symbol not expected in Print"; break;
+			case 28: s = "this symbol not expected in Assignment"; break;
+			case 29: s = "invalid EqlOp"; break;
+			case 30: s = "invalid RelOp"; break;
+			case 31: s = "invalid AddOp"; break;
+			case 32: s = "invalid UnaryExp"; break;
+			case 33: s = "invalid MulOp"; break;
+			case 34: s = "invalid Factor"; break;
+
+			default: s = "error " + n; break;
+		}
+		StoreError(line, col, s);
+		count++;
+	}
+
+	public static void SemErr (int line, int col, int n) {
+		StoreError(line, col, ("error " + n));
+		count++;
+	}
+
+	public static void Error (int line, int col, string s) {
+		StoreError(line, col, s);
+		count++;
+	}
+
+	public static void Error (string s) {
+		if (mergeErrors) mergedList.WriteLine(s); else Console.WriteLine(s);
+		count++;
+	}
+
+	public static void Warn (int line, int col, string s) {
+		StoreError(line, col, s);
+		warns++;
+	}
+
+	public static void Warn (string s) {
+		if (mergeErrors) mergedList.WriteLine(s); else Console.WriteLine(s);
+		warns++;
+	}
+
+	public static void Exception (string s) {
+		Console.WriteLine(s);
+		System.Environment.Exit(1);
+	}
+
+} // end Errors
+
+} // end namespace
---
+++ Scanner.cs
@@ -0,0 +1,308 @@
+
+using System;
+using System.IO;
+using System.Collections;
+using System.Text;
+
+namespace Calc {
+
+public class Token {
+	public int kind;    // token kind
+	public int pos;     // token position in the source text (starting at 0)
+	public int col;     // token column (starting at 0)
+	public int line;    // token line (starting at 1)
+	public string val;  // token value
+	public Token next;  // AW 2003-03-07 Tokens are kept in linked list
+}
+
+public class Buffer {
+	public const char EOF = (char)256;
+	static byte[] buf;
+	static int bufLen;
+	static int pos;
+
+	public static void Fill (Stream s) {
+		bufLen = (int) s.Length;
+		buf = new byte[bufLen];
+		s.Read(buf, 0, bufLen);
+		pos = 0;
+	}
+
+	public static int Read () {
+		if (pos < bufLen) return buf[pos++];
+		else return EOF;                          /* pdt */
+	}
+
+	public static int Peek () {
+		if (pos < bufLen) return buf[pos];
+		else return EOF;                          /* pdt */
+	}
+
+	/* AW 2003-03-10 moved this from ParserGen.cs */
+	public static string GetString (int beg, int end) {
+		StringBuilder s = new StringBuilder(64);
+		int oldPos = Buffer.Pos;
+		Buffer.Pos = beg;
+		while (beg < end) { s.Append((char)Buffer.Read()); beg++; }
+		Buffer.Pos = oldPos;
+		return s.ToString();
+	}
+
+	public static int Pos {
+		get { return pos; }
+		set {
+			if (value < 0) pos = 0;
+			else if (value >= bufLen) pos = bufLen;
+			else pos = value;
+		}
+	}
+
+} // end Buffer
+
+public class Scanner {
+	const char EOL = '\n';
+	const int  eofSym = 0;
+	const int charSetSize = 256;
+	const int maxT = 26;
+	const int noSym = 26;
+	// terminals
+	const int EOF_SYM = 0;
+	const int number_Sym = 1;
+	const int identifier_Sym = 2;
+	const int quit_Sym = 3;
+	const int equal_Sym = 4;
+	const int semicolon_Sym = 5;
+	const int print_Sym = 6;
+	const int comma_Sym = 7;
+	const int barbar_Sym = 8;
+	const int andand_Sym = 9;
+	const int plus_Sym = 10;
+	const int minus_Sym = 11;
+	const int bang_Sym = 12;
+	const int true_Sym = 13;
+	const int false_Sym = 14;
+	const int lparen_Sym = 15;
+	const int rparen_Sym = 16;
+	const int star_Sym = 17;
+	const int slash_Sym = 18;
+	const int percent_Sym = 19;
+	const int less_Sym = 20;
+	const int lessequal_Sym = 21;
+	const int greater_Sym = 22;
+	const int greaterequal_Sym = 23;
+	const int equalequal_Sym = 24;
+	const int bangequal_Sym = 25;
+	const int NOT_SYM = 26;
+	// pragmas
+
+	static short[] start = {
+	  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+	  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+	  0, 21,  0,  0,  0, 15,  7,  0, 11, 12, 13,  9,  4, 10,  0, 14,
+	  1,  1,  1,  1,  1,  1,  1,  1,  1,  1,  0,  3, 22, 20, 23,  0,
+	  0,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,
+	  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  0,  0,  0,  0,  0,
+	  0,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,
+	  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  2,  0,  5,  0,  0,  0,
+	  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+	  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+	  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+	  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+	  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+	  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+	  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+	  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,
+	  -1};
+
+
+	static Token t;          // current token
+	static char ch;          // current input character
+	static int pos;          // column number of current character
+	static int line;         // line number of current character
+	static int lineStart;    // start position of current line
+	static int oldEols;      // EOLs that appeared in a comment;
+	static BitArray ignore;  // set of characters to be ignored by the scanner
+
+	static Token tokens;     // the complete input token stream
+	static Token pt;         // current peek token
+
+	public static void Init (string fileName) {
+		FileStream s = null;
+		try {
+			s = new FileStream(fileName, FileMode.Open, FileAccess.Read, FileShare.Read);
+			Init(s);
+		} catch (IOException) {
+			Console.WriteLine("--- Cannot open file {0}", fileName);
+			System.Environment.Exit(1);
+		} finally {
+			if (s != null) s.Close();
+		}
+	}
+
+	public static void Init (Stream s) {
+		Buffer.Fill(s);
+		pos = -1; line = 1; lineStart = 0;
+		oldEols = 0;
+		NextCh();
+		ignore = new BitArray(charSetSize+1);
+		ignore[' '] = true;  // blanks are always white space
+		ignore[0] = true; ignore[1] = true; ignore[2] = true; ignore[3] = true; 
+		ignore[4] = true; ignore[5] = true; ignore[6] = true; ignore[7] = true; 
+		ignore[8] = true; ignore[9] = true; ignore[10] = true; ignore[11] = true; 
+		ignore[12] = true; ignore[13] = true; ignore[14] = true; ignore[15] = true; 
+		ignore[16] = true; ignore[17] = true; ignore[18] = true; ignore[19] = true; 
+		ignore[20] = true; ignore[21] = true; ignore[22] = true; ignore[23] = true; 
+		ignore[24] = true; ignore[25] = true; ignore[26] = true; ignore[27] = true; 
+		ignore[28] = true; ignore[29] = true; ignore[30] = true; ignore[31] = true; 
+		
+		//--- AW: fill token list
+		tokens = new Token();  // first token is a dummy
+		Token node = tokens;
+		do {
+			node.next = NextToken();
+			node = node.next;
+		} while (node.kind != eofSym);
+		node.next = node;
+		node.val = "EOF";
+		t = pt = tokens;
+	}
+
+	static void NextCh() {
+		if (oldEols > 0) { ch = EOL; oldEols--; }
+		else {
+			ch = (char)Buffer.Read(); pos++;
+			// replace isolated '\r' by '\n' in order to make
+			// eol handling uniform across Windows, Unix and Mac
+			if (ch == '\r' && Buffer.Peek() != '\n') ch = EOL;
+			if (ch == EOL) { line++; lineStart = pos + 1; }
+		}
+
+	}
+
+
+	static bool Comment0() {
+		int level = 1, line0 = line, lineStart0 = lineStart;
+		NextCh();
+		if (ch == '/') {
+			NextCh();
+			for(;;) {
+				if (ch == 10) {
+					level--;
+					if (level == 0) { oldEols = line - line0; NextCh(); return true; }
+					NextCh();
+				} else if (ch == Buffer.EOF) return false;
+				else NextCh();
+			}
+		} else {
+			if (ch == EOL) { line--; lineStart = lineStart0; }
+			pos = pos - 2; Buffer.Pos = pos+1; NextCh();
+		}
+		return false;
+	}
+
+
+	static void CheckLiteral() {
+		switch (t.val) {
+			case "quit": t.kind = quit_Sym; break;
+			case "print": t.kind = print_Sym; break;
+			case "true": t.kind = true_Sym; break;
+			case "false": t.kind = false_Sym; break;
+			default: break;
+		}
+	}
+
+	/* AW Scan() renamed to NextToken() */
+	static Token NextToken() {
+		while (ignore[ch]) NextCh();
+		if (ch == '/' && Comment0()) return NextToken();
+		t = new Token();
+		t.pos = pos; t.col = pos - lineStart + 1; t.line = line;
+		int state = start[ch];
+		StringBuilder buf = new StringBuilder(16);
+		buf.Append(ch); NextCh();
+		switch (state) {
+			case -1: { t.kind = eofSym; goto done; } // NextCh already done /* pdt */
+			case 0: { t.kind = noSym; goto done; }   // NextCh already done
+			case 1:
+				if ((ch >= '0' && ch <= '9')) { buf.Append(ch); NextCh(); goto case 1; }
+				else { t.kind = number_Sym; goto done; }
+			case 2:
+				if ((ch >= '0' && ch <= '9'
+				  || ch >= 'A' && ch <= 'Z'
+				  || ch >= 'a' && ch <= 'z')) { buf.Append(ch); NextCh(); goto case 2; }
+				else { t.kind = identifier_Sym; t.val = buf.ToString(); CheckLiteral(); return t; }
+			case 3:
+				{ t.kind = semicolon_Sym; goto done; }
+			case 4:
+				{ t.kind = comma_Sym; goto done; }
+			case 5:
+				if (ch == '|') { buf.Append(ch); NextCh(); goto case 6; }
+				else { t.kind = noSym; goto done; }
+			case 6:
+				{ t.kind = barbar_Sym; goto done; }
+			case 7:
+				if (ch == '&') { buf.Append(ch); NextCh(); goto case 8; }
+				else { t.kind = noSym; goto done; }
+			case 8:
+				{ t.kind = andand_Sym; goto done; }
+			case 9:
+				{ t.kind = plus_Sym; goto done; }
+			case 10:
+				{ t.kind = minus_Sym; goto done; }
+			case 11:
+				{ t.kind = lparen_Sym; goto done; }
+			case 12:
+				{ t.kind = rparen_Sym; goto done; }
+			case 13:
+				{ t.kind = star_Sym; goto done; }
+			case 14:
+				{ t.kind = slash_Sym; goto done; }
+			case 15:
+				{ t.kind = percent_Sym; goto done; }
+			case 16:
+				{ t.kind = lessequal_Sym; goto done; }
+			case 17:
+				{ t.kind = greaterequal_Sym; goto done; }
+			case 18:
+				{ t.kind = equalequal_Sym; goto done; }
+			case 19:
+				{ t.kind = bangequal_Sym; goto done; }
+			case 20:
+				if (ch == '=') { buf.Append(ch); NextCh(); goto case 18; }
+				else { t.kind = equal_Sym; goto done; }
+			case 21:
+				if (ch == '=') { buf.Append(ch); NextCh(); goto case 19; }
+				else { t.kind = bang_Sym; goto done; }
+			case 22:
+				if (ch == '=') { buf.Append(ch); NextCh(); goto case 16; }
+				else { t.kind = less_Sym; goto done; }
+			case 23:
+				if (ch == '=') { buf.Append(ch); NextCh(); goto case 17; }
+				else { t.kind = greater_Sym; goto done; }
+
+		}
+		done:
+		t.val = buf.ToString();
+		return t;
+	}
+
+	/* AW 2003-03-07 get the next token, move on and synch peek token with current */
+	public static Token Scan () {
+		t = pt = t.next;
+		return t;
+	}
+
+	/* AW 2003-03-07 get the next token, ignore pragmas */
+	public static Token Peek () {
+		do {                      // skip pragmas while peeking
+			pt = pt.next;
+		} while (pt.kind > maxT);
+		return pt;
+	}
+
+	/* AW 2003-03-11 to make sure peek start at current scan position */
+	public static void ResetPeek () { pt = t; }
+
+} // end Scanner
+
+} // end namespace
